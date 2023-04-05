fc33ff07d37ad54747ae4eeb13095b34f4181486
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-04-05 08:56:58 +0000

    Adding presigned URL

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2ODEzMTQ0NzN9fX1dfQ__&Signature=d0CFUAG4IK2QN97SzDKLBAoVQKDgs01N-9fz0eN~dGs84OmJvvifedIhNZwxg0dTdmYmyvn4VMcZid5vHj91VMqY~3B29gSTXhibVQ8S5qb57oTYO4p2nmRaIBGfODPbVOTJ0MkSMl1XyeVnvrT9Nk~owtgMHm-RMTuP0PvgHgoSTZuI-ByZ4IxYbScFVfjY4F7ik2JtACYSN0l6lQAew8LbprLDFUIGyVodq8FS3o~FRfE5C8AISmMnEUI7KnVyd1id7J3ncOyPmdJh3mPGw8-RDskXGuss6v1tQPWYvFosXLKi3VG7DcOQ4Zv62VVgEagMkG86CynYRRM-qf96ZA__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
 MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
 TARGET_FOLDER=""             # where all files should end up
 
